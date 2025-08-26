cb9feb1e3e8d2dad07bd3b20ea04caf9cd681d3c
Sebastián Pulgarín C <sebastian.pulgarinc@upb.edu.co>
2025-08-26 11:27:30 -0500

    Clase del 26 de Agosto

--- Ejercicios/Ejercicio 1.py
+++ Ejercicios/Ejercicio 1.py
@@ -12,12 +12,28 @@ Peso = float(Peso)
 Estatura = input("Ingrese su estatura en metros:")
 Estatura = float(Estatura)
 
-print ("Los datos ingresados son: Peso =", Peso,"Kg.", "Estarura =", Estatura,"m")
+print ("Los datos ingresados son: Peso =", Peso,"Kg.", "Estatura =", Estatura,"m")
 print ("Se calculará su índice de Masa Corporal (IMC) como: Peso [Kg] / (Estatura)^2")
 
 IMC = Peso/(Estatura)**2
 IMC = float(IMC)
 
-print ("Su índice de masa corporal (IMC) es:", IMC)
+print (f"Su índice de masa corporal es {IMC:0.2f}")
 
-
+if IMC >= 40:
+    print (f"Paciente {Nombre}, según la OMS, tienes obesidad extrema tipo III.")
+else:
+    if IMC >= 35:
+        print (f"Paciente {Nombre}, según la OMS, tienes obesidad extrema tipo II.")
+    else:
+        if IMC >= 30:
+            print (f"Paciente {Nombre}, según la OMS, tienes obesidad extrema tipo I.")
+        else:
+            if IMC >= 25:
+                print (f"Paciente {Nombre}, según la OMS, tienes sobrepeso.")
+            else:
+                if IMC >= 18.5:
+                    print (f"Paciente {Nombre}, según la OMS, estás en un peso adecuado.")
+                else:
+                    print (f"Paciente {Nombre}, según la OMS, tienes bajo peso.")
+ 
